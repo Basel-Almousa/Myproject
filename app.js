0a0ddc3edeb63cd9895ae44a701432ee5d869b12
Basel-Almousa <basel.almuusa@gmail.com>
2021-06-20 19:06:12 +0300

    ,

---
+++ app.js
@@ -0,0 +1,13 @@
+
+alert("Answer with 'YES' or 'NO' !");
+let x = prompt('have you ever known that Hot water will turn into ice faster than cold water?.');
+console.log(x);
+
+let y = prompt('have you ever known that The Mona Lisa has no eyebrows?.');
+console.log(y);
+
+let z = prompt('have you ever known that The strongest muscle in the body is the tongue?.');
+console.log(z);
+
+let m = prompt('have you ever known that A snail can sleep for three years?');
+console.log(m);
